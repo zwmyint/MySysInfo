4f11d47d1cfc5e2557e9ff5e3a423baf6ed4fd49
ZAW WIN MYINT <45326766+zwmyint@users.noreply.github.com>
2023-12-21 23:19:23 +0800

    commit 20231221

---
+++ MyEnums.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static MyEnums.ConsoleApp.Helper;
+
+Console.WriteLine("Hello, World!");
+
+
+FileAccessEnum _FileAccessEnum = FileAccessEnum.Read | FileAccessEnum.Write;
+Console.WriteLine(_FileAccessEnum);
+
+var _StatusDes = GetDescriptions<Status>();
+foreach (var item in _StatusDes)
+{
+    Console.WriteLine(item);
+}
+
+foreach (var item in Enum.GetValues(typeof(WeekDays)))
+{
+    Console.WriteLine($"{item} == {(int)item}");
+}
+
+var orderStatus = Status.Pending;
+Console.WriteLine(orderStatus.IsInProcess());
+
+Console.WriteLine(LogLevel.Error + ": An error occurred");
+
+// Nullable enum
+TrafficLight? currentLight = null;
+Console.WriteLine(currentLight != null ? "NullTrue": "NullFalse");
+
